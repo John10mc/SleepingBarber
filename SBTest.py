3b504b29f3277d4e4721f7688e6572aae9f4656b
John McLaughlin <John10mc@gmail.com>
2020-03-25 19:57:52 +0000

    Working for different num of barbers and capacity

--- SBTest.py
+++ SBTest.py
@@ -7,81 +7,89 @@ def main():
     print("Barber shop opened at: ", time.ctime())
     shop = BarberShop()
     shop.open()
-    time.sleep(5)
-    for i in range(40):
+    for i in range(1, 51):
+        time.sleep(1)
         shop.newCustomer(Customer(i))
-        time.sleep(3)
+        print("Customer {} has entered the shop".format(i))
+    shop.close()
 
 
 class BarberShop():
 
     def __init__(self):
+        self.names = ["Bob", "Billy", "Carl", "Ben"]
         self.barbers = []
-        self.numberOfBarbers = 1
-        self.capacity = 15
+        self.numberOfBarbers = 4
+        self.isOpen = True
+        self.capacity = 5
         self.waitingRoom = queue.Queue()
         self.availableBarbers = self.numberOfBarbers
+        self.lock = threading.Lock()
 
-    def startDay(self, e):
-        barber = Barber(threading.current_thread(), e, self.waitingRoom)
+    def startDay(self):
+        barber = Barber(threading.current_thread().getName())
         self.barbers.append(barber)
-        barber.goToWork()
-
-    def con(self):
-        time.sleep(5)
-        print(self.barbers[0])
-        if not self.barbers[0].isAwake():
-            self.barbers[0].wakeUp()
+        while self.isOpen:
+            self.lock.acquire()
+            if self.waitingRoom.empty():
+                print("Barber {} has gone to sleep.".format(barber.name))
+                self.lock.release()
+                barber.sleep()
+                print("Barber {} has woke up.".format(barber.name))
+            else:
+                self.availableBarbers -= 1
+                self.lock.release()
+                barber.cutHair(self.waitingRoom.get())
+                self.availableBarbers += 1
 
     def open(self):
         for i in range(self.numberOfBarbers):
-            e = threading.Event()
-            self.barbers.append(threading.Thread(target=self.startDay, args=(e,)).start())
+            threading.Thread(target=self.startDay, name=self.names[i]).start()
+
+    def close(self):
+        while self.waitingRoom.qsize() > 0:
+            pass
+        self.isOpen = False
+        for barber in self.barbers:
+            if not barber.isAwake():
+                barber.wakeUp()
+        print("Barber shop is closing at: {}".format(time.ctime()))
+
 
     def newCustomer(self, customer):
-        if self.availableBarbers > 0:
+        if self.waitingRoom.qsize() == 0 and self.availableBarbers > 0:
             for barber in self.barbers:
                 if not barber.isAwake():
-                    self.availableBarbers - 1
-                    self.availableBarbers += barber.wakeUp(customer)
-        elif len(self.waitingRoom) < self.capacity:
+                    self.waitingRoom.put(customer)
+                    barber.wakeUp()
+                    break
+        elif self.waitingRoom.qsize() < self.capacity:
+            print("Customer {} waits in the waiting room at: {}".format(customer.name, time.ctime()))
             self.waitingRoom.put(customer)
         else:
-            print("customer leaves")
+            print("Customer {} leaving.".format(customer.name))
 
 class Barber():
 
-    def __init__(self, name, e, waitingRoom):
+    def __init__(self, name):
         self.name = name
-        self.event = e
-        self.waitingRoom = waitingRoom
+        self.event = threading.Event()
         print("Barber {} started work at: {}".format(self.name, time.ctime()))
 
-    def goToWork(self):
-        while True:
-            if self.waitingRoom.empty():
-                self.sleep()
-            else:
-                self.cutHair(waitingRoom.get())
-
     def sleep(self):
-        #print(self.name, "sleep")
+        self.event.clear()
         self.event.wait()
 
-    def wakeUp(self, customer):
-        print(self.name, "awake")
+    def wakeUp(self):
         self.event.set()
-        return self.cutHair(customer)
-
 
     def isAwake(self):
         return self.event.is_set()
 
     def cutHair(self, customer):
         print("Barber {} has started cutting {}'s hair at: {}".format(self.name, customer.name, time.ctime()))
-        self.awake = True
-        time.sleep(random.randint(0, 10))
-        return 1
+        time.sleep(random.randint(1, 7))
+        print("Barber {} has finished cutting {}'s hair at: {}".format(self.name, customer.name, time.ctime()))
 
 
 class Customer():
